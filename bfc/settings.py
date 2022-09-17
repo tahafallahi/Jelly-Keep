745eda3b31577a4b77d596ebf59e85b4e0536b0b
Taha Fallahi <113535640+tahafallahi@users.noreply.github.com>
2022-09-18 02:23:53 +0430

    Update settings.py

--- bfc/settings.py
+++ bfc/settings.py
@@ -88,12 +88,9 @@ WSGI_APPLICATION = 'bfc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
-    }
-}
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
