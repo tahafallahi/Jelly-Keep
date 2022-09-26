5cb82feaa87bbfd5287ecee5bb3e89f371fbfdce
Taha Fallahi <tahafallahi.blob@gmail.com>
2022-09-26 09:53:02 +0330

    moved from bootstrap to purecss

--- word/urls.py
+++ word/urls.py
@@ -6,5 +6,6 @@ app_name = 'word'
 urlpatterns = [
     path('', views.WordListView.as_view(), name='list'),
     path('create/', views.WordCreateView.as_view(), name='create'),
+    path('delete/list', views.WordDeleteListView.as_view(), name='delete_list'),
     path('delete/<int:pk>/', views.WordDeleteView.as_view(), name='delete'),
     ]
